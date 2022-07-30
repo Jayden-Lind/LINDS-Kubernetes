ce72686e4e6bfffa0bd44e98c9fb6b652d14b353
Jayden Lind <70465314+Jayden-Lind@users.noreply.github.com>
2022-07-30 21:06:45 +1000

    Update app-deployment.sh

--- app-deployment.sh
+++ app-deployment.sh
@@ -3,3 +3,4 @@ kubectl apply -f deployment.yml
 kubectl apply -f tautulli.yml
 kubectl apply -f linds-virtualserver.yml
 kubectl apply -f smb-deployment.yml
+curl -skSL https://raw.githubusercontent.com/kubernetes-csi/csi-driver-smb/v1.8.0/deploy/install-driver.sh | bash -s v1.8.0 --
