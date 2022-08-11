569145c7f2be0275eb8c9deaff57681f7d926dc2
Jayden Lind <jaydenlind@gmail.com>
2022-08-11 11:40:32 +1000

    Factorio + MetalLB

--- app-deployment.sh
+++ app-deployment.sh
@@ -4,3 +4,4 @@ kubectl apply -f tautulli.yml
 kubectl apply -f linds-virtualserver.yml
 kubectl apply -f smb-deployment.yml
 curl -skSL https://raw.githubusercontent.com/kubernetes-csi/csi-driver-smb/v1.8.0/deploy/install-driver.sh | bash -s v1.8.0 --
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.4/config/manifests/metallb-native.yaml
