36588db086d80f369916b9e91e4151602afbbfba
Jayden Lind <jaydenlind@gmail.com>
2022-08-14 23:02:38 +1000

    Added Duin to update images automatically

--- app-deployment.sh
+++ app-deployment.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
+curl -skSL https://raw.githubusercontent.com/kubernetes-csi/csi-driver-smb/v1.8.0/deploy/install-driver.sh | bash -s v1.8.0 --
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.4/config/manifests/metallb-native.yaml
 kubectl apply -f deployment.yml
 kubectl apply -f tautulli.yml
 kubectl apply -f linds-virtualserver.yml
-kubectl apply -f smb-deployment.yml
-curl -skSL https://raw.githubusercontent.com/kubernetes-csi/csi-driver-smb/v1.8.0/deploy/install-driver.sh | bash -s v1.8.0 --
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.4/config/manifests/metallb-native.yaml
+kubectl apply -f duin.yml
+kubectl apply -f factorio.yml
