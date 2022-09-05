8f74ee4dd2f8f93cc28f15bca8309bfef674d0e6
Jayden Lind <jaydenlind@gmail.com>
2022-09-05 22:01:02 +1000

    Update readme

--- app-deployment.sh
+++ app-deployment.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 curl -skSL https://raw.githubusercontent.com/kubernetes-csi/csi-driver-smb/v1.8.0/deploy/install-driver.sh | bash -s v1.8.0 --
+kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/main/deployments/common/crds/k8s.nginx.org_virtualserverroutes.yaml
+kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/main/deployments/common/crds/k8s.nginx.org_virtualservers.yaml
+kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/main/deployments/common/crds/k8s.nginx.org_transportservers.yaml
+kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/main/deployments/common/crds/k8s.nginx.org_policies.yaml
+kubectl apply -f nginx-config.yml
+kubectl apply -f nginx-controller.yml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.4/config/manifests/metallb-native.yaml
 kubectl apply -f deployment.yml
 kubectl apply -f tautulli.yml
