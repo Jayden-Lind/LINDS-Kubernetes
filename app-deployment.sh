50fdbf2cac9c11979869852d49d1327fd0f1d09e
Jayden Lind <jaydenlind@gmail.com>
2022-09-18 20:44:17 +1000

    Update metallb + unifi + cleanup

--- app-deployment.sh
+++ app-deployment.sh
@@ -6,10 +6,13 @@ kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/m
 kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/main/deployments/common/crds/k8s.nginx.org_policies.yaml
 kubectl apply -f nginx-config.yml
 kubectl apply -f nginx-controller.yml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.4/config/manifests/metallb-native.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.5/config/manifests/metallb-native.yaml
 kubectl apply -f deployment.yml
 kubectl apply -f tautulli.yml
 kubectl apply -f linds-virtualserver.yml
 kubectl apply -f duin.yml
 kubectl apply -f factorio.yml
 kubectl apply -f postgresql.yml
+kubectl apply -f unifi.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.6.1/aio/deploy/recommended.yaml
+kubectl apply -f kube-dashboard.yaml
