1828afedb9d0ae1e0865d287297b0c2c5ea7469f
Jayden Lind <jaydenlind@gmail.com>
2024-05-04 12:45:38 +1000

    fix: update nginx

--- app-deployment.sh
+++ app-deployment.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-CALICO_VERSION=v3.27.2
-NGINX_VERSION=v3.3.1
+CALICO_VERSION=v3.27.3
+NGINX_VERSION=v3.5.0
 
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/$CALICO_VERSION/manifests/tigera-operator.yaml
 kubectl create -f calico
@@ -12,30 +12,25 @@ kubectl label nodes linds-kube-01 datacenter=linds
 kubectl label nodes linds-kube-02 datacenter=linds
 
 if ! test -f /usr/local/bin/kubectl-calico; then
-    curl -L https://github.com/projectcalico/calico/releases/download/v3.26.3/calicoctl-linux-amd64 -o /usr/local/bin/kubectl-calico && chmod +x /usr/local/bin/kubectl-calico
+    curl -L https://github.com/projectcalico/calico/releases/download/$CALICO_VERSION/calicoctl-linux-amd64 -o /usr/local/bin/kubectl-calico && chmod +x /usr/local/bin/kubectl-calico
 fi
 
 kubectl apply -f coredns.yml
 
 #argo-cd
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+# kubectl create namespace argocd
+# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-if ! test -f /usr/local/bin/argocd; then
-    curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
-    sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
-    rm argocd-linux-amd64
-fi
+# if ! test -f /usr/local/bin/argocd; then
+#     curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+#     sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+#     rm argocd-linux-amd64
+# fi
 
 ##NGINX
 kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/$NGINX_VERSION/deployments/common/ns-and-sa.yaml
 kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/$NGINX_VERSION/deployments/rbac/rbac.yaml
-kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/$NGINX_VERSION/deployments/rbac/ap-rbac.yaml
-kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/$NGINX_VERSION/deployments/rbac/apdos-rbac.yaml
-kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/$NGINX_VERSION/deployments/common/crds/k8s.nginx.org_virtualserverroutes.yaml
-kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/$NGINX_VERSION/deployments/common/crds/k8s.nginx.org_virtualservers.yaml
-kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/$NGINX_VERSION/deployments/common/crds/k8s.nginx.org_transportservers.yaml
-kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/$NGINX_VERSION/deployments/common/crds/k8s.nginx.org_policies.yaml
+kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/$NGINX_VERSION/deploy/crds.yaml
 kubectl apply -f nginx
 
 
