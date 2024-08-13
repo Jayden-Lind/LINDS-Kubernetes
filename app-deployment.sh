ab8d78127dccaf2d973c3e28486ac8451e38500f
Jayden Lind <jaydenlind@gmail.com>
2024-08-13 23:53:02 +1000

    fix: updates + fixes for ubuntu clusters

--- app-deployment.sh
+++ app-deployment.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-CALICO_VERSION=v3.28.0
-NGINX_VERSION=v3.5.0
+CALICO_VERSION=v3.28.1
+NGINX_VERSION=v3.6.1
 
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/$CALICO_VERSION/manifests/tigera-operator.yaml
 kubectl create -f calico
