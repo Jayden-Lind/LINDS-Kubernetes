c8556c35a00631dfb2eb5b36a74a9554dddefc13
Jayden Lind <jaydenlind@gmail.com>
2025-07-25 20:25:03 +1000

    fix: various fixes

--- app-deployment.sh
+++ app-deployment.sh
@@ -1,27 +1,27 @@
 #! /bin/bash
 
 # Calico (Networking)
-kubectl create namespace tigera-operator
 helm repo add projectcalico https://docs.tigera.io/calico/charts
-helm install calico projectcalico/tigera-operator \
+helm upgrade --install calico projectcalico/tigera-operator \
   --namespace tigera-operator \
   --create-namespace \
   --version v3.30.2 \
-  --set calicoNetwork.containerIPForwarding=Enabled \
-  --set calicoNetwork.bgp=Enabled \
-  --set calicoNetwork.ipPools[0].allowedUses[0]=Workload \
-  --set calicoNetwork.ipPools[0].allowedUses[1]=Tunnel \
-  --set calicoNetwork.ipPools[0].name=default-ipv4-ippool \
-  --set calicoNetwork.ipPools[0].disableBGPExport=false \
-  --set calicoNetwork.ipPools[0].blockSize=26 \
-  --set calicoNetwork.ipPools[0].cidr=10.244.0.0/16 \
-  --set calicoNetwork.ipPools[0].encapsulation=IPIPCrossSubnet \
-  --set calicoNetwork.ipPools[0].natOutgoing=Enabled \
-  --set calicoNetwork.ipPools[0].nodeSelector="all()"
+  --set installation.calicoNetwork.containerIPForwarding=Enabled \
+  --set installation.calicoNetwork.bgp=Enabled \
+  --set installation.calicoNetwork.linuxDataplane=BPF \
+  --set installation.calicoNetwork.ipPools[0].name=default-ipv4-ippool \
+  --set installation.calicoNetwork.ipPools[0].cidr=10.244.0.0/16 \
+  --set installation.calicoNetwork.ipPools[0].blockSize=26 \
+  --set installation.calicoNetwork.ipPools[0].encapsulation=IPIPCrossSubnet \
+  --set installation.calicoNetwork.ipPools[0].natOutgoing=Enabled \
+  --set installation.calicoNetwork.ipPools[0].allowedUses[0]=Workload \
+  --set installation.calicoNetwork.ipPools[0].disableBGPExport=false \
+  --set installation.calicoNetwork.ipPools[0].nodeSelector="all()"
 
 
 #argo-cd
 kubectl create namespace argocd
+kubectl apply -f base/linds-secret.yml
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 if ! test -f /usr/local/bin/argocd; then
@@ -36,9 +36,11 @@ kubectl taint node jd-kube-01 node-role.kubernetes.io/control-plane:NoSchedule-
 kubectl create -f applications
 
 kubectl create -k base
+echo "NOW APPLYING AGAIN"
+kubectl apply -k base
 
 # Reboot ubuntu nodes after provisioning
 
 # Deleting kube-proxy to prevent collision with calico
-kubectl -n kube-system delete ds kube-proxy
-kubectl -n kube-system delete cm kube-proxy
+#kubectl -n kube-system delete ds kube-proxy
+#kubectl -n kube-system delete cm kube-proxy
