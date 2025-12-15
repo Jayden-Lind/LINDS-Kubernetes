0d63345623da4645e5f2ccbd6c96f4a2a5ae2c5a
jayden <jaydenlind@gmail.com>
2025-12-16 00:21:06 +1100

    fix: patch config maps, argocd, install metrics server

--- app-deployment.sh
+++ app-deployment.sh
@@ -29,12 +29,31 @@ helm upgrade --install calico projectcalico/tigera-operator \
   --set installation.calicoNetwork.ipPools[1].allowedUses[0]=LoadBalancer \
   --set installation.calicoNetwork.ipPools[1].nodeSelector="all()"
 
+kubectl apply -f - <<EOF
+kind: ConfigMap
+apiVersion: v1
+metadata:
+  name: kubernetes-services-endpoint
+  namespace: tigera-operator
+data:
+  KUBERNETES_SERVICE_HOST: "10.0.53.11"
+  KUBERNETES_SERVICE_PORT: "6443"
+EOF
 
 #argo-cd
 kubectl create namespace argocd
 kubectl apply -f base/linds-secret.yml
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl patch configmap argocd-cmd-params-cm -n argocd --type merge -p '{"data":{"server.insecure":"true"}}'
+kubectl -n argocd patch configmap argocd-cm \
+  --type merge \
+  -p '{
+    "data": {
+      "timeout.reconciliation": "3600s",
+      "timeout.reconciliation.jitter": "300s"
+    }
+  }'
+
 
 
 if ! test -f /usr/local/bin/argocd; then
